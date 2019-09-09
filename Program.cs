925801ec0b3826155f114cf340c4cf4ff713b87c
pruth <pruth@DESKTOP-MECPLOA>
2019-09-09 19:19:47 -0400

    Removed console

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+//using System.Diagnostics;
+
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Hello World!");
+            //Console.ReadKey();
+        }
+    }
+}
